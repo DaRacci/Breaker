41fd77d4d2e234d33689acd09d8981e7bb3729f3
ASangarin <ariasangarin@gmail.com>
2020-07-29 19:26:09 +0200

    Command Trigger now has generic placeholders

--- src/main/java/com/asangarin/breaker/triggers/CommandTrigger.java
+++ src/main/java/com/asangarin/breaker/triggers/CommandTrigger.java
@@ -1,6 +1,7 @@
 package com.asangarin.breaker.triggers;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
@@ -13,31 +14,47 @@ import me.clip.placeholderapi.PlaceholderAPI;
 public class CommandTrigger implements BreakTrigger {
 	private String command;
 	private boolean console;
-	
+
 	CommandTrigger register(String cmd, boolean cs) {
 		command = cmd;
 		console = cs;
 		return this;
 	}
-	
+
 	@Override
 	public String type() {
 		return "command";
 	}
-	
+
 	@Override
 	public void execute(Player player, Block block) {
-		if(console) Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), parsedCommand(player, command));
-		player.performCommand(parsedCommand(player, command));
+		if (console)
+			Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(),
+					parsedCommand(player, block.getLocation(), command));
+		player.performCommand(parsedCommand(player, block.getLocation(), command));
 	}
 
 	@Override
 	public BreakTrigger register(ConfigurationSection c) {
 		return register(c.getString("command", "say no command!"), c.getBoolean("console", false));
 	}
-	
-	public String parsedCommand(Player player, String cmd) {
-		if(!Breaker.plugin.isLoaded("PlaceholderAPI")) return cmd;
+
+	public String parsedCommand(Player player, Location loc, String cmd) {
+		cmd = cmd.replace("{player}", player.getName())
+				.replace("{blockx}", "" + loc.getX())
+				.replace("{blocky}", "" + loc.getY())
+				.replace("{blockz}", "" + loc.getZ())
+				.replace("{playerx}", "" + player.getLocation().getX())
+				.replace("{playery}", "" + player.getLocation().getY())
+				.replace("{playerz}", "" + player.getLocation().getZ())
+				.replace("{blockxint}", "" + loc.getBlockX())
+				.replace("{blockyint}", "" + loc.getBlockY())
+				.replace("{blockzint}", "" + loc.getBlockZ())
+				.replace("{playerxint}", "" + player.getLocation().getBlockX())
+				.replace("{playeryint}", "" + player.getLocation().getBlockY())
+				.replace("{playerzint}", "" + player.getLocation().getBlockZ());
+		if (!Breaker.plugin.isLoaded("PlaceholderAPI"))
+			return cmd;
 		return PlaceholderAPI.setPlaceholders(player, cmd);
 	}
 }
