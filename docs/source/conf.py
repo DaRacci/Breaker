cb5a710b426587e35aaf1982b7fd9a36eebe0f52
ASangarin <ariasangarin@gmail.com>
2022-01-05 15:45:39 +0100

    docs update (still very wip)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Breaker'
-copyright = '2021, Aria Sangarin'
-author = 'Aria Sangarin'
+project = 'Breaker Official Docs'
+copyright = '2022, ASangarin'
+author = 'ASangarin'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0'
+release = '1'
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,8 +30,7 @@ release = '2.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,9 +46,32 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'groundwork'
+html_theme = 'sphinx_rtd_theme'
+
+html_logo = "img/docs_logo.png"
+
+html_theme_options = {
+    # if we have a html_logo below, this shows /only/ the logo with no title text
+    "logo_only": True,
+    # Collapse navigation (False makes it tree-like)
+    "collapse_navigation": False,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_css_files = [
+    'css/algolia.css',
+    'https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css',
+    "css/custom.css",
+]
+
+html_js_files = [
+    "js/custom.js",
+    ('https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js', {'defer': 'defer'}),
+    ('js/algolia.js', {'defer': 'defer'})
+]
+
+file_insertion_enabled = False
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,15 +1,46 @@
-Official Breaker Documentation
-==============================
+Breaker Docs - v2.0
+=================================================
+(Docs v1.0)
 
 .. toctree::
-   :maxdepth: 2
-   :caption: Contents:
+   :maxdepth: 1
+   :caption: Getting Started
+   
+   quickstart/installation
+   quickstart/configuration
+   quickstart/important
 
+.. toctree::
+   :maxdepth: 1
+   :caption: Mechanics
+   
+   mechanics/hardness
+   mechanics/providers
+   mechanics/states
+   mechanics/triggers
 
+.. toctree::
+   :maxdepth: 1
+   :caption: States
+   
+   states/enchant
+   states/experience
+   states/effect
+   states/sneak
+   states/time
+   states/tool
+   states/weather
+   states/world
+   states/inair
+   states/inwater
+   states/external/index
 
-Indices and tables
-==================
+.. toctree::
+   :maxdepth: 1
+   :caption: API
+   
+   api/providers
+   api/states
+   api/triggers
 
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
+yare yare
