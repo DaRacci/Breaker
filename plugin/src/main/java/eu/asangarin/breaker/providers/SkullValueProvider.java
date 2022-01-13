cf21ed408c451d66180fe4750adf5836497499d2
ASangarin <ariasangarin@gmail.com>
2022-01-13 19:29:19 +0100

    2.2.1 Fixed the server kicking players when trying to mine interactive blocks

--- plugin/src/main/java/eu/asangarin/breaker/providers/SkullValueProvider.java
+++ plugin/src/main/java/eu/asangarin/breaker/providers/SkullValueProvider.java
@@ -2,17 +2,28 @@ package eu.asangarin.breaker.providers;
 
 import eu.asangarin.breaker.api.IBlockProvider;
 import io.lumine.mythic.lib.MythicLib;
+import io.lumine.mythic.utils.Schedulers;
+import io.lumine.mythic.utils.promise.Promise;
 import org.bukkit.block.Block;
 import org.bukkit.block.Skull;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.concurrent.ExecutionException;
 
 public class SkullValueProvider implements IBlockProvider {
 	@Override
 	public Collection<String> getKeys(Block block) {
-		if (block.getState() instanceof Skull)
-			return Collections.singletonList("SKULL_" + MythicLib.plugin.getVersion().getWrapper().getSkullValue(block));
-		return Collections.emptyList();
+		Promise<Collection<String>> promise = Schedulers.sync().supply(() -> {
+			if (block.getState() instanceof Skull)
+				return Collections.singletonList("SKULL_" + MythicLib.plugin.getVersion().getWrapper().getSkullValue(block));
+			return Collections.emptyList();
+		});
+
+		try {
+			return promise.get();
+		} catch (InterruptedException | ExecutionException e) {
+			return Collections.emptyList();
+		}
 	}
 }
